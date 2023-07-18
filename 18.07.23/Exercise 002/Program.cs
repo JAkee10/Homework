2cb27f1723d408614051739dba828e84c376f58c
JAkee <JakePomg94@gmail.com>
2023-07-18 04:00:33 +0300

    Выполнил второе задание

---
+++ 18.07.23/Exercise 002/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 8: Напишите программу, 
+которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+
+Console.Clear();
+
+int i = 2;
+Console.Write("Enter positive number: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N > 0)
+{
+    while (i < N)
+    {
+        Console.Write($"{i} ");
+        i++;
+    }
+}
+else
+{
+    Console.Write("Restart and enter positive number please!");
+}
