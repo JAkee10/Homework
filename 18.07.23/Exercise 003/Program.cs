b7479b6297ac5e376843ff4f1d49f769baf3c220
JAkee <JakePomg94@gmail.com>
2023-07-18 04:12:01 +0300

    Выполнил третье задание (бонусное, с урока)

---
+++ 18.07.23/Exercise 003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Enter three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+int lastDigit = 0;
+
+if (number / 100 >= 1)
+{
+    lastDigit = number % 10;
+    Console.Write($"Last digit in the number it's: {lastDigit}");
+}
+else
+{
+    Console.Write("Restart and enter three-digit number please!");
+}
