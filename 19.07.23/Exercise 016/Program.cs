bbedb4c2dbbbc7ec8a796a56c2c30fc51ac2170c
JAkee <JakePomg94@gmail.com>
2023-07-19 03:14:34 +0300

    Сделал ДЗ за второй семинар

---
+++ 19.07.23/Exercise 016/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+Console.Write("Enter number : ");
+string stringNum = Console.ReadLine();
+int num = int.Parse(stringNum);
+ 
+if (num / 100 != 0) Console.Write($"Third digit in {num} -> {stringNum[2]}");
+else Console.Write($"Number {num} don't have third digit");
---
+++ 19.07.23/Exercise 017/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+// Задача 15: Напишите программу, которая принимает 
+// на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Enter number of day of the week: ");
+int numberOfDayOfWeek = int.Parse(Console.ReadLine());
+
+if (numberOfDayOfWeek > 0 && numberOfDayOfWeek < 8)
+{
+    if (numberOfDayOfWeek == 6 || numberOfDayOfWeek == 7) 
+    Console.WriteLine($"{numberOfDayOfWeek} -> Holiday!!!");
+    else Console.WriteLine($"{numberOfDayOfWeek} -> Go to work D:");
+}
+else Console.WriteLine("Enter number of day of the week please");
