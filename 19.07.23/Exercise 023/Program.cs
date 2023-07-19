3f7feed6ec559541658b8e29ec073de954ddca1b
JAkee <JakePomg94@gmail.com>
2023-07-19 18:21:40 +0300

    Выполнил задачу 23

---
+++ 19.07.23/Exercise 023/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.Write("Enter N number : ");
+double N = Convert.ToDouble(Console.ReadLine());
+
+if (N > 0)
+{
+    Console.Write($"{N} -> ");
+    for (int i = 1; i < N; i++)
+        Console.Write($"{Math.Pow(i, 3)}, ");
+    Console.Write(Math.Pow(N, 3));
+}
+else
+{
+    Console.WriteLine("Enter positive number!");
+}
