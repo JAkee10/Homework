f3d5ab0021493a95e28219c74fed742ad75100eb
JAkee <JakePomg94@gmail.com>
2023-07-25 00:54:17 +0300

    Выполнил задачу 36

---
+++ 25.07.23/Exercise 36/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, стоящих 
+// на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+Console.Write("Enter length of array : ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+}
+Console.Write($"Count of elements on odd position in array [{String.Join(", ", array)}] is {SumOfOddArrayEl(array)}");
+
+int SumOfOddArrayEl(int[] arr)
+{
+    int count = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+        count += arr[i];
+    return count;
+}
