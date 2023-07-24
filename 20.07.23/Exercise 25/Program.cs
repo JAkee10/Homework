3078a83b2c13cf81daafc5856de930c178d887b5
JAkee <JakePomg94@gmail.com>
2023-07-24 16:26:18 +0300

    Выполнил задачу 25

---
+++ 20.07.23/Exercise 25/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Enter A number : ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter B number : ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double res = Pow(a, b);
+Console.WriteLine(res);
+
+
+
+double Pow(double num1, double num2)
+{
+    double result = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        result *= num1;
+    }
+    return result;
+}
