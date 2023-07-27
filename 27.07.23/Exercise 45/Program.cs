5aed18c0fdd919af2ab249ee003aaf2439a3a837
JAkee <JakePomg94@gmail.com>
2023-07-27 11:06:41 +0300

    Выполнил задачу 45

---
+++ 27.07.23/Exercise 45/Program.cs
@@ -0,0 +1,24 @@
+﻿/*    Задача 45: Напишите прошрамму, которая будет создавать копию заданного 
+массива с помощью поэлементного копирования    */
+
+Console.Clear();
+int[] copyArray = GetArray();
+int[] pasteArray = CopyPasteArray(copyArray);
+Console.WriteLine($"Массив copyArray : [{String.Join(", ", copyArray)}], успешно скопирован в массив pasteArray : [{String.Join(", ", pasteArray)}]");
+
+int[] GetArray()
+{
+    Console.Write($"Введите числа для массива через запятую ',' : ");
+    int[] nums = Console.ReadLine().Split(',')
+                             .Select(e => int.Parse(e))
+                             .ToArray();
+    return nums;
+}
+
+int [] CopyPasteArray(int[] copy)
+{
+    int[] paste = new int[copy.Length];
+    for (int i = 0; i < copy.Length; i++)
+        paste[i] = copy[i];
+    return paste;
+}
