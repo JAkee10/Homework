6d86b8a4d4b638c091f034484cc0676b08d6a752
JAkee <JakePomg94@gmail.com>
2023-08-31 17:27:15 +0300

    Выполнил задачу 66

---
+++ 31.08.2023/Exercise 66/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int GetSum(int M, int N)
+{
+    if (M == N)
+    {
+        return N;
+    }
+    return M + GetSum(M + 1, N);
+}
+
+
+
+Console.Clear();
+
+Console.Write($"Введите первое число: ");
+int minNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите второе число: ");
+int maxNumber = Convert.ToInt32(Console.ReadLine());
+if (minNumber > maxNumber)
+{
+    int temp = minNumber;
+    minNumber = maxNumber;
+    maxNumber = temp;
+}
+
+Console.WriteLine();
+
+Console.WriteLine($"Результат: {GetSum(minNumber, maxNumber)}");
