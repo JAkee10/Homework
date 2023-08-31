471835f692b855eba839f9c860cda67c0014975d
JAkee <JakePomg94@gmail.com>
2023-08-31 17:26:49 +0300

    Выполнил задачу 64

---
+++ 31.08.2023/Exercise 64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+string GetNumbers(int N)
+{
+    if (N == 1)
+    {
+        return "1";
+    }
+    return N.ToString() + ", " + GetNumbers(N - 1);
+}
+
+
+
+Console.Clear();
+
+Console.Write($"Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Результат: {GetNumbers(n)}");
